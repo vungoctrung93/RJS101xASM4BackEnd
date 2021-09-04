b6dbfa702a3ad2bea9d85983da00b05797cfe322
Vu Ngoc Trung <vungoctrung93@gmail.com>
2021-09-05 02:26:32 +0900

    fix test code, add validate for salary cal

--- controllers/staffsSalaryController.js
+++ controllers/staffsSalaryController.js
@@ -4,7 +4,13 @@ var { DEPARTMENTS } = require('../data/departments')
 exports.getStaffsSalary = (req, res, next) => {
     //employee
     const staffsSalary = STAFFS.map((staff)=>{
-        staff.salary = Math.round((staff.salaryScale * 3000000) + (staff.overTime * 200000));
+        if(staff.annualLeave === undefined) {
+            staff.annualLeave = 0;
+        }
+        if(staff.salaryScale === undefined) {
+            staff.salaryScale = 1;
+        }
+        staff.salary = Math.round(staff.salaryScale * 3000000 + staff.overTime * 200000 );
         return staff;
     })
     res.status(200).json(staffsSalary);
