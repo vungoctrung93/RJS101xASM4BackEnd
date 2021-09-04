77278b3c125d2b44a6877e7621ab8cef9f2a7a9b
Vu Ngoc Trung <vungoctrung93@gmail.com>
2021-09-04 22:40:04 +0900

    add test method, reverse staff salary result

--- controllers/staffsSalaryController.js
+++ controllers/staffsSalaryController.js
@@ -4,18 +4,8 @@ var { DEPARTMENTS } = require('../data/departments')
 exports.getStaffsSalary = (req, res, next) => {
     //employee
     const staffsSalary = STAFFS.map((staff)=>{
-        const salary = Math.round((staff.salaryScale * 3000000) + (staff.overTime * 200000));
-        const staffSalary = {
-            id: staff.id,
-            name: staff.name,
-            doB: staff.doB,
-            startDate: staff.startDate,
-            departmentId: staff.departmentId,
-            annualLeave: staff.annualLeave,
-            image: staff.image,
-            salary : salary,
-        }
-        return staffSalary;
+        staff.salary = Math.round((staff.salaryScale * 3000000) + (staff.overTime * 200000));
+        return staff;
     })
     res.status(200).json(staffsSalary);
 };
